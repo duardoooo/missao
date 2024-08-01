6435aa57d67b85043c9a9c3b905be2bc84fd1261
duardoooo <162701038+duardoooo@users.noreply.github.com>
2024-08-01 11:46:11 -0300

    shift

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.queryselector(".caixa-Principal");
+const caixaPerguntas = document.queryselector(".caixa-perguntas");
+const caixaAlternativas = document.queryselector(".caixa-alternativas");
+const caixaResultado = document.queryselector(".caixa-resultado");
+const textoPrincipal = document.queryselector(".caixa-resultado");
+
+const perguntas =[
+    {
+        enunciado: 
+        alternativas: [
+        ]
+    },
+{
+    enunciado: 
+    alternativas: [
+    ]
+},
+{
+    enunciado: 
+    alternativas: [
+    ]
+}
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appenChild(botaoAlternativas);
+
+    }
+}
+mostraPergunta();
